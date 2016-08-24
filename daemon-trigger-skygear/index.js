0f624ec95a7e541f46c6a4f8edf8d2a224415378
Jason Wu <jasonkit321@gmail.com>
2016-08-24 18:00:55 +0800

    Add heartbeat and the corresponding log for daemon-trigger-skygear

--- daemon-trigger-skygear/index.js
+++ daemon-trigger-skygear/index.js
@@ -7,17 +7,42 @@ const apiKey = process.argv[2] || '';
 
 const channel = '&chima-open-door';
 
+var heartbeatTimer = null;
+var heartbeatCounter = 0;
+const heartbeatChannel = '&chima-open-door-heartbeat';
+const heartbeatInterval = 1000 * 60;
 function onConnectionOpen() {
   console.log("daemon-trigger-skygear: connection open");
+  
+  if (heartbeatTimer == null) {
+    heartbeatTimer = setInterval(function() {
+	skygear.pubsub.publish(heartbeatChannel, heartbeatCounter+1);
+    }, heartbeatInterval);
+  }  
 }
 
 function onConnectionClose() {
   console.log("daemon-trigger-skygear: connection close");
+
+  if (heartbeatTimer != null) {
+    clearInterval(heartbeatTimer);
+    heartbeatTimer = null;
+  }
 }
 
 function onReceiveOpenDoor(data) {
   console.log('daemon-trigger-skygear: open door');
   exec(`curl localhost:8090 --header 'X-Source: Skygear'`);
+}
+
+function onHeartbeat(data) {
+  if (data != (heartbeatCounter + 1)) {
+    console.log('daemon-trigger-skygear: unexpected heartbeat counter:' + data + ", expecting " + (heartbeatCounter+1));
+  } else if ((data % 60) == 0){
+    console.log('daemon-trigger-skygear: connection alive');
+  }
+
+  heartbeatCounter = data;
 }
 
 skygear.config({
@@ -28,5 +53,6 @@ skygear.config({
     skygear.pubsub.onOpen(onConnectionOpen);
     skygear.pubsub.onClose(onConnectionClose);
     skygear.on(channel, onReceiveOpenDoor);
+    skygear.on(heartbeatChannel, onHeartbeat);
   });
 });
