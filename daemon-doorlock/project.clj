fe544eef18881214150b734a414bac4722d69884
Ben Cheng <bencheng@oursky.com>
2017-12-29 17:31:26 +0800

    Add nanoleaf trigger when open door

--- daemon-doorlock/project.clj
+++ daemon-doorlock/project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.385"]
                  [com.taoensso/timbre "4.7.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [http-kit "2.2.0"]
                  ]
   :aot :all
--- daemon-doorlock/src/com/oursky/doorlock/core.clj
+++ daemon-doorlock/src/com/oursky/doorlock/core.clj
@@ -2,8 +2,9 @@
   (:require [taoensso.timbre :as log]
             [clojure.java.shell :refer [sh]]
             [clojure.core.async :refer [<! >! >!! alts! go-loop chan timeout]]
+            [clojure.data.json :as json]
             [org.httpkit.server :refer [run-server]]
-            )
+            [org.httpkit.client :as http])
   (:gen-class))
 
 ; unlock triggering channel
@@ -15,16 +16,25 @@
   (sh "gpio" "mode" "0" "up")
   (sh "gpio" "mode" "1" "out")
 
+  ; define the http options to control nanoleaf
+  (defn options [effect]
+    {:url "http://192.168.2.198:16021/api/v1/sNKAKzAzGZMqGrVCQadvOZi0IWh6cUdS/effects"
+     :timeout 500
+     :method :put
+     :body (json/write-str {:select effect})})
+
   ; listen on unlock-chan for unlock events
   ; if a new unlock event is revieved before the 3000ms timeout, the door is kept open.
   (go-loop [unlock nil]
            (when unlock
+             (http/request (options "Access granted"))
              (sh "gpio" "write" "1" "1")
              (loop [[trigger _] [unlock nil]]
                (when trigger
                  (log/info (str "Unlock triggered by " (:source trigger)))
                  (recur (alts! [unlock-chan (timeout 3000)]))))
              (sh "gpio" "write" "1" "0")
+             (http/request (options "Oursky"))
              (log/info "Door Locked"))
            (recur (<! unlock-chan)))
 
